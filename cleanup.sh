6545fb3f7b11f7174b6aaec91a9c7947dad34c5c
gwenola <gwenola.lerou@gmail.com>
2024-03-22 17:51:39 -0400

    data dir creation ok

--- cleanup.sh
+++ cleanup.sh
@@ -6,5 +6,6 @@ source srcs/.env
 # Edit the LOGIN variable in server_name
 sed "s/${LOGIN}/login/g" srcs/requirements/nginx/conf/nginx.conf
 sed "s/${LOGIN}/login/g" srcs/requirements/nginx/Dockerfile
+# To keep for evaluation !!!
 #rm -rf srcs/.env .env.make
 rm -rf .env.make
