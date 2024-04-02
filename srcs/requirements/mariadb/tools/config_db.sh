231efda49fe64307922d77a18b295c74e427a617
laouede <gwenola.leroux@gmail.com>
2024-04-02 08:34:28 -0400

    minor changes to work on campus VM

--- srcs/requirements/mariadb/tools/config_db.sh
+++ srcs/requirements/mariadb/tools/config_db.sh
@@ -8,59 +8,59 @@ main() {
 }
 
 initialize_database() {
-    echo "-------------- Database Initialization --------------"
+	echo "-------------- Database Initialization --------------"
 	# Check if the database already exists.
-    if [ -d "/var/lib/mysql/${DB_NAME}" ]; then
-        echo "Database ${DB_NAME} already exists. Skipping configuration step."
-    else
-        configure_conf_file
-        start_mariadb
-        create_database_and_user
-        shutdown_mariadb
-    fi
+	if [ -d "/var/lib/mysql/${DB_NAME}" ]; then
+		echo "Database ${DB_NAME} already exists. Skipping configuration step."
+	else
+		configure_conf_file
+		start_mariadb
+		create_database_and_user
+		shutdown_mariadb
+	fi
 	echo "-------------- DB Initialization DONE ---------------"
-    start_mariadb_safe
+	start_mariadb_safe
 }
 
 configure_conf_file() {
-    if ! grep -q "^port\s*=\s*3306" /etc/mysql/mariadb.conf.d/50-server.cnf; then
-        echo "port = 3306" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-    fi
+	if ! grep -q "^port\s*=\s*3306" /etc/mysql/mariadb.conf.d/50-server.cnf; then
+		echo "port = 3306" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+	fi
 
-    if ! grep -q "bind-address = 0.0.0.0" /etc/mysql/mariadb.conf.d/50-server.cnf; then
-        sed -i 's/bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
-    fi
+	if ! grep -q "bind-address = 0.0.0.0" /etc/mysql/mariadb.conf.d/50-server.cnf; then
+		sed -i 's/bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
+	fi
 }
 
 start_mariadb() {
 	echo "----------------- Starting MariaDB ------------------"
-    mkdir -p /var/run/mysqld
-    chown -R mysql:mysql /var/run/mysqld
-    service mysql start
-    sleep 7
+	mkdir -p /var/run/mysqld
+	chown -R mysql:mysql /var/run/mysqld
+	service mysql start
+	sleep 7
 }
 
 create_database_and_user() {
-    echo "------------------ Table creation -------------------"
-    SQL_COMMANDS="${DB_NAME}.sql"
-    echo "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;" > "$SQL_COMMANDS"
-    echo "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASS}';" >> "$SQL_COMMANDS"
-    echo "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASS}' WITH GRANT OPTION;" >> "$SQL_COMMANDS"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';" >> "$SQL_COMMANDS"
-    echo "FLUSH PRIVILEGES;" >> "$SQL_COMMANDS"
-    cat "$SQL_COMMANDS"
-    mysql < "$SQL_COMMANDS" || { echo "Failed to initialize database"; exit 1; }
+	echo "------------------ Table creation -------------------"
+	SQL_COMMANDS="${DB_NAME}.sql"
+	echo "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;" > "$SQL_COMMANDS"
+	echo "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASS}';" >> "$SQL_COMMANDS"
+	echo "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASS}' WITH GRANT OPTION;" >> "$SQL_COMMANDS"
+	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';" >> "$SQL_COMMANDS"
+	echo "FLUSH PRIVILEGES;" >> "$SQL_COMMANDS"
+	cat "$SQL_COMMANDS"
+	mysql < "$SQL_COMMANDS" || { echo "Failed to initialize database"; exit 1; }
 }
 
 shutdown_mariadb() {
-    echo "------------------ Server Shutdown ------------------"
-    mysqladmin -u root -p"$DB_ROOT" -S /var/run/mysqld/mysqld.sock shutdown
-    sleep 7
+	echo "------------------ Server Shutdown ------------------"
+	mysqladmin -u root -p"$DB_ROOT" -S /var/run/mysqld/mysqld.sock shutdown
+	sleep 7
 }
 
 start_mariadb_safe() {
-    echo "----------------- Start mysqld_safe -----------------"
-    exec mysqld_safe
+	echo "----------------- Start mysqld_safe -----------------"
+	exec mysqld_safe
 }
 
 main
--- srcs/requirements/wordpress/tools/config_wp.sh
+++ srcs/requirements/wordpress/tools/config_wp.sh
@@ -4,108 +4,108 @@
 set -e
 
 main() {
-    initialize_wordpress
+	initialize_wordpress
 }
 
 initialize_wordpress() {
-    echo "-------------- Wordpress Initialization -------------"
+	echo "-------------- Wordpress Initialization -------------"
 
-    wait_for_db
+	wait_for_db
 
-    # Check if the WordPress configuration file exists.
-    if [ -f "${WP_PATH}/wp-config.php" ]; then
-        echo "Config file already exists. Skipping the configuration step."
-    else
-        configure_conf_file
-        configure_wordpress
-        create_site
-        create_default_user
-        install_theme
-        create_post
-    fi
+	# Check if the WordPress configuration file exists.
+	if [ -f "${WP_PATH}/wp-config.php" ]; then
+		echo "Config file already exists. Skipping the configuration step."
+	else
+		configure_conf_file
+		configure_wordpress
+		create_site
+		create_default_user
+		install_theme
+		create_post
+	fi
 
-    echo "-------------- WP Initialization DONE ---------------"
-    start_php_fpm
+	echo "-------------- WP Initialization DONE ---------------"
+	start_php_fpm
 }
 
 wait_for_db() {
-    echo "--------------- Waiting for MariaDB... --------------"
-    for i in {1..30}; do
-        if mariadb -h"${DB_HOST}" -u"${DB_USER}" -p"${DB_PASS}" "${DB_NAME}" &>/dev/null; then
-            echo "MariaDB is ready."
-            return
-        fi
-        sleep 1
-    done
-    echo "MariaDB connection failed."
-    exit 1
+	echo "--------------- Waiting for MariaDB... --------------"
+	for i in {1..30}; do
+		if mariadb -h"${DB_HOST}" -u"${DB_USER}" -p"${DB_PASS}" "${DB_NAME}" &>/dev/null; then
+			echo "MariaDB is ready."
+			return
+		fi
+		sleep 1
+	done
+	echo "MariaDB connection failed."
+	exit 1
 }
 
 configure_conf_file() {
-    if ! grep -q "listen = 0.0.0.0:9000" /etc/php/7.3/fpm/pool.d/www.conf; then
-        echo "listen = 0.0.0.0:9000" >> /etc/php/7.3/fpm/pool.d/www.conf
-    fi
+	if ! grep -q "listen = 0.0.0.0:9000" /etc/php/7.3/fpm/pool.d/www.conf; then
+		echo "listen = 0.0.0.0:9000" >> /etc/php/7.3/fpm/pool.d/www.conf
+	fi
 
-    if ! grep -q "clear_env = no" /etc/php/7.3/fpm/pool.d/www.conf; then
-        echo "clear_env = no" >> /etc/php/7.3/fpm/pool.d/www.conf
-    fi
+	if ! grep -q "clear_env = no" /etc/php/7.3/fpm/pool.d/www.conf; then
+		echo "clear_env = no" >> /etc/php/7.3/fpm/pool.d/www.conf
+	fi
 }
 
 configure_wordpress() {
-    echo "----------------- 1.Wordpress config ----------------"
-    wp-cli.phar config create --allow-root \
-        --dbname="$DB_NAME" \
-        --dbuser="$DB_USER" \
-        --dbpass="$DB_PASS" \
-        --dbhost="$DB_HOST" \
-        --path="$WP_PATH" || { echo "WordPress configuration failed"; exit 1; }
+	echo "----------------- 1.Wordpress config ----------------"
+	wp-cli.phar config create --allow-root \
+		--dbname="$DB_NAME" \
+		--dbuser="$DB_USER" \
+		--dbpass="$DB_PASS" \
+		--dbhost="$DB_HOST" \
+		--path="$WP_PATH" || { echo "WordPress configuration failed"; exit 1; }
 }
 
 create_site() {
-    echo "------------------ 2.Site creation ------------------"
-    wp-cli.phar core install --allow-root \
-        --url="$DOMAIN_NAME" \
-        --title="$WP_NAME" \
-        --admin_user="$WP_ADMIN" \
-        --admin_password="$WP_ADMIN_PASS" \
-        --admin_email="$WP_ADMIN_EMAIL" \
-        --path="$WP_PATH" || { echo "Site creation failed"; exit 1; }
+	echo "------------------ 2.Site creation ------------------"
+	wp-cli.phar core install --allow-root \
+		--url="$DOMAIN_NAME" \
+		--title="$WP_NAME" \
+		--admin_user="$WP_ADMIN" \
+		--admin_password="$WP_ADMIN_PASS" \
+		--admin_email="$WP_ADMIN_EMAIL" \
+		--path="$WP_PATH" || { echo "Site creation failed"; exit 1; }
 }
 
 create_default_user() {
-    echo "-------------- 3.Default user creation --------------"
-    wp-cli.phar user create --allow-root \
-        "$WP_USER" \
-        "$WP_USER_EMAIL" \
-        --role=contributor \
-        --display_name="$WP_USER" \
-        --user_pass="$WP_USER_PASS" \
-        --path="$WP_PATH" || { echo "Default user creation failed"; exit 1; }
+	echo "-------------- 3.Default user creation --------------"
+	wp-cli.phar user create --allow-root \
+		"$WP_USER" \
+		"$WP_USER_EMAIL" \
+		--role=contributor \
+		--display_name="$WP_USER" \
+		--user_pass="$WP_USER_PASS" \
+		--path="$WP_PATH" || { echo "Default user creation failed"; exit 1; }
 }
 
 install_theme() {
-    echo "----------------- 4.Wordpress theme -----------------"
-    # agama bravada zakra
-    wp-cli.phar theme install zakra --activate --allow-root \
-        --path="$WP_PATH" || { echo "Theme installation failed"; exit 1; }
-    wp-cli.phar theme status zakra --allow-root \
-        --path="$WP_PATH" || { echo "Theme status check failed"; exit 1; }
+	echo "----------------- 4.Wordpress theme -----------------"
+	# agama bravada zakra
+	wp-cli.phar theme install zakra --activate --allow-root \
+		--path="$WP_PATH" || { echo "Theme installation failed"; exit 1; }
+	wp-cli.phar theme status zakra --allow-root \
+		--path="$WP_PATH" || { echo "Theme status check failed"; exit 1; }
 }
 
 create_post() {
-    echo "------------------ 5.Post creation ------------------"
-    wp-cli.phar post create --allow-root \
-        --post_author="$WP_ADMIN" \
-        --post_title='Fun fact about Inception!' \
-        --post_content='A unique aspect of INCEPTION is its musical score composed by Hans Zimmer. The song "Non, Je Ne Regrette Rien" by Edith Piaf, used as a plot device in the film, is actually integrated into the design. Zimmer slowed down its elements to create the iconic, slow, and reverberating sounds that pervade the movie, particularly in dream sequences. This creative approach adds depth to the exploration of time and memory, blurring the lines between different levels of consciousness.' \
-        --post_status=publish \
-        --comment_status=open \
-        --path="$WP_PATH" || { echo "Post creation failed"; exit 1; }
+	echo "------------------ 5.Post creation ------------------"
+	wp-cli.phar post create --allow-root \
+		--post_author="$WP_ADMIN" \
+		--post_title='Fun fact about Inception!' \
+		--post_content='A unique aspect of INCEPTION is its musical score composed by Hans Zimmer. The song "Non, Je Ne Regrette Rien" by Edith Piaf, used as a plot device in the film, is actually integrated into the design. Zimmer slowed down its elements to create the iconic, slow, and reverberating sounds that pervade the movie, particularly in dream sequences. This creative approach adds depth to the exploration of time and memory, blurring the lines between different levels of consciousness.' \
+		--post_status=publish \
+		--comment_status=open \
+		--path="$WP_PATH" || { echo "Post creation failed"; exit 1; }
 }
 
 start_php_fpm() {
-    echo "------------------- Start php-fpm -------------------"
-    /usr/sbin/php-fpm7.3 -F
+	echo "------------------- Start php-fpm -------------------"
+	/usr/sbin/php-fpm7.3 -F
 }
 
 main
