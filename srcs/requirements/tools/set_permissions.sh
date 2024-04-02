9af66028307ddac309ef844cb3da1e9997748488
LaOuede <gwenola.leroux@gmail.com>
2024-04-02 10:03:05 -0400

    set_permissions.sh modified

---
+++ srcs/requirements/tools/set_permissions.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Set owner
+sudo chown -R gle-roux:gle-roux /42-Inception
+
+# Set file permissions
+sudo chmod 664 Makefile
+sudo chmod 664 srcs/docker-compose.yml
+sudo chmod 664 srcs/.env
+sudo chmod 664 srcs/requirements/mariadb/Dockerfile
+sudo chmod 664 srcs/requirements/nginx/Dockerfile
+sudo chmod 664 srcs/requirements/wordpress/Dockerfile
+# sudo chmod 777 Makefile
+# sudo chmod 777 srcs/docker-compose.yml
+# sudo chmod 777 srcs/.env
+# sudo chmod 777 srcs/requirements/mariadb/Dockerfile
+# sudo chmod 777 srcs/requirements/nginx/Dockerfile
+# sudo chmod 777 srcs/requirements/wordpress/Dockerfile
+
+# Set executable permissions for scripts
+sudo chmod 775 srcs/requirements/mariadb/tools/config_db.sh
+sudo chmod 775 srcs/requirements/nginx/conf/nginx.conf
+sudo chmod 775 srcs/requirements/wordpress/tools/config_wp.sh
+sudo chmod 775 srcs/requirements/tools/cleanup.sh
+sudo chmod 775 srcs/requirements/tools/setup.sh
+
+# Set directory permissions
+sudo chmod 775 srcs/
+sudo chmod 775 srcs/requirements/
+sudo chmod 775 srcs/requirements/mariadb/
+sudo chmod 775 srcs/requirements/nginx/
+sudo chmod 775 srcs/requirements/wordpress/
+sudo chmod 775 srcs/requirements/tools/
+sudo chmod 775 srcs/requirements/mariadb/tools/
+sudo chmod 775 srcs/requirements/nginx/conf/
+sudo chmod 775 srcs/requirements/wordpress/tools/
